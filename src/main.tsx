f8d4c5b3ae97c50ae3a16b26b9d617f2a1ec8a51
Myukee@osu <1789446861@qq.com>
2022-09-20 22:07:32 +0800

    v0.2.0-alpha

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import dva from 'dva';
 import { createBrowserHistory } from 'history';
+import { version } from '../package.json';
 import { Routes } from './routes';
 import models from './models';
 import { initI18n } from './i18n';
@@ -7,8 +8,11 @@ import './index.d.ts';
 import 'antd/dist/antd.css';
 import './reset.css';
 import './index.css';
+import { prodLog } from './utils/log';
 
 initI18n();
+
+prodLog(version);
 
 const app = dva({
   history: createBrowserHistory(),
